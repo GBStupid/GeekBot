fc52ee4aee4395587083bb9c3d0adf5c783f0ea5
auth-xyz <authenticover@gmail.com>
2025-06-02 00:34:38 -0400

    feat: Changed message to gif

--- src/events/on_message.py
+++ src/events/on_message.py
@@ -7,7 +7,7 @@ async def setup(bot):
         content = message.content.lower()
 
         if content in ["brb", "be right back"]:
-            await message.channel.send("this isn't an airport, you don't need to announce your departure :3")
+            await message.channel.send("https://tenor.com/view/this-isnt-an-airport-no-gif-26083761")
         elif content in ["im back", "i'm back"]:
             await message.channel.send(f"hi back, i'm {bot.user.name}")
         await bot.process_commands(message)
