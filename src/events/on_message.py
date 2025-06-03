fc21d47b30b897f320885202e606cf61ca57abb7
RuriYS <ruriyoshinova@proton.me>
2025-06-03 13:47:16 +0000

    refactor(on_message): format

--- src/events/on_message.py
+++ src/events/on_message.py
@@ -13,7 +13,9 @@ class OnMessageCog(commands.Cog):
         content = message.content.lower()
 
         if content in ["brb", "be right back"]:
-            await message.channel.send("https://tenor.com/view/this-isnt-an-airport-no-gif-26083761")
+            await message.channel.send(
+                "https://tenor.com/view/this-isnt-an-airport-no-gif-26083761"
+            )
         elif content in ["im back", "i'm back"]:
             await message.channel.send(f"hi back, i'm {self.bot.user.name}")
         await self.bot.process_commands(message)
