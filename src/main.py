9365306db8b947350911f7b79c259cb547b9c1f3
RuriYS <ruriyoshinova@proton.me>
2025-06-03 11:42:26 +0000

    refractor(main): fix some mess

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-import discord
-import asyncio
 import os
+import sys
+import asyncio
+import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -14,14 +15,16 @@ bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 
 async def load_cogs():
-    if not os.path.exists('src'):
+    if not os.path.exists("src"):
         raise FileNotFoundError(
-            'Source directory does not exist, make sure to run this outside src.')
+            "Source directory does not exist, make sure to run this outside src."
+        )
 
     for folder in ["commands", "events"]:
         for filename in os.listdir(f"src/{folder}"):
             if filename.endswith(".py") and not filename.startswith("_"):
                 await bot.load_extension(f"{folder}.{filename[:-3]}")
+
 
 async def main():
     if not token:
@@ -31,11 +34,9 @@ async def main():
     await bot.start(token)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('⛔ Shutting down')
-        exit(0)
-    except Exception as exc:
-        raise Exception(exc)
+        print("⛔ Shutting down")
+        sys.exit(0)
